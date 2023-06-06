cc2c599a013ad2f25148af140b909a27838e9e15
Juan David <juanguerrero09mc@gmail.com>
2023-06-06 12:51:48 +0200

    change gitingore and remove inquire

--- hidraulica_canales.py
+++ hidraulica_canales.py
@@ -1,6 +1,4 @@
 # Librerías
-import pprint
-import inquirer
 from secciones import SeccionRectangular, SeccionTrapezoidal, SeccionTriangular, SeccionCircular
 from sympy import Symbol
 
@@ -46,59 +44,59 @@ def calcular_seccion(seccion, calculo, n_input, So_input, Q_input=None, b_input=
 
 
 
-secciones = [
-    "Cuadrada",
-    "Triangular",
-    "Trapezoidal",
-    # "Circular",
-]
+# secciones = [
+#     "Cuadrada",
+#     "Triangular",
+#     "Trapezoidal",
+#     # "Circular",
+# ]
 
-def elegir_seccion():
+# def elegir_seccion():
     
-    opciones_seccion = [
-    inquirer.List(
-        "seccion",
-        message="Selecciona el tipo de sección a analizar",
-        choices=secciones,
-    ),
-]
-    answers = inquirer.prompt(opciones_seccion)
-    seccion_elegida = answers["seccion"]
+#     opciones_seccion = [
+#     inquirer.List(
+#         "seccion",
+#         message="Selecciona el tipo de sección a analizar",
+#         choices=secciones,
+#     ),
+# ]
+#     answers = inquirer.prompt(opciones_seccion)
+#     seccion_elegida = answers["seccion"]
 
-    n_input = float(input("Defina el coeficiente de rugosidad: "))
-    So_input = float(input("Defina la pendiente longitudinal: "))
-    Q_input = float(input("Defina el caudal: "))
-
-
-    if seccion_elegida == "Cuadrada":
-        b_input = float(input("Defina la base: "))
-        # y_input = float(input("Defina la altura: "))
-        seccion = SeccionRectangular(n_input, So_input, Q_input, b_input)
+#     n_input = float(input("Defina el coeficiente de rugosidad: "))
+#     So_input = float(input("Defina la pendiente longitudinal: "))
+#     Q_input = float(input("Defina el caudal: "))
 
 
-    elif seccion_elegida == "Triangular":
-        z_input = float(input("Defina la base: "))
-        # y_input = float(input("Defina la altura: "))
-        seccion = SeccionTriangular(n_input, So_input, Q_input, z_input)
+#     if seccion_elegida == "Cuadrada":
+#         b_input = float(input("Defina la base: "))
+#         # y_input = float(input("Defina la altura: "))
+#         seccion = SeccionRectangular(n_input, So_input, Q_input, b_input)
+
+
+#     elif seccion_elegida == "Triangular":
+#         z_input = float(input("Defina la base: "))
+#         # y_input = float(input("Defina la altura: "))
+#         seccion = SeccionTriangular(n_input, So_input, Q_input, z_input)
 
 
 
-    elif seccion_elegida == "Trapezoidal":
-        b_input = float(input("Defina la base: "))
-        z_input = float(input("Defina la pendiente_lateral: "))
-        # y_input = float(input("Defina la altura: "))
-        seccion = SeccionTrapezoidal(n_input, So_input, Q_input, b_input, z_input)
+#     elif seccion_elegida == "Trapezoidal":
+#         b_input = float(input("Defina la base: "))
+#         z_input = float(input("Defina la pendiente_lateral: "))
+#         # y_input = float(input("Defina la altura: "))
+#         seccion = SeccionTrapezoidal(n_input, So_input, Q_input, b_input, z_input)
 
 
-    # elif seccion_elegida == "Circular":
-    #     seccion = SeccionCircular()
-    #     do_input = float(input("Defina el diámetro: "))
-    #     y_input = float(input("Defina la altura: "))
-    #     seccion.set_diametro(do_input)
-    #     seccion.set_altura(y_input)
+#     # elif seccion_elegida == "Circular":
+#     #     seccion = SeccionCircular()
+#     #     do_input = float(input("Defina el diámetro: "))
+#     #     y_input = float(input("Defina la altura: "))
+#     #     seccion.set_diametro(do_input)
+#     #     seccion.set_altura(y_input)
 
-    seccion.calc_yn()
-    return seccion.__dict__, seccion_elegida
+#     seccion.calc_yn()
+#     return seccion.__dict__, seccion_elegida
 
 
 
