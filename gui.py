e9a4a2a130cfa0b7bb33297598f853af67fe36c8
Juan David <juanguerrero09mc@gmail.com>
2023-06-14 16:15:47 +0200

    gui's separated

--- gui.py
+++ gui.py
@@ -1,11 +1,7 @@
-from sympy import Symbol
 import tkinter as tk
-import turtle
 from tkinter import ttk
-from math import sqrt, atan, degrees, radians
-from draws import draw_Trapezoid_channel, draw_circle, draw_triangle, draw_Rectangle, get_img_from_draw
 from custom_components import CustomEntry
-from PIL import Image, ImageTk
+from gui_open_flow import OpenFlowGui
 
 import sys
 sys.path.append('modules')
@@ -16,238 +12,47 @@ from excel_export import generate_report
 # Ahora puedes usar las funciones y variables definidas en modulo.py
 
 
-class MyEntries(tk.Frame):
-    def __init__(self, master, values, section = None, calculation=None):
-        super().__init__(master)
-        self.values = values 
-        self.section = section
-        definition = {
-            'n': "Manning's Coef.",
-            'Q': 'Flow Rate [m3/s]',
-            'yn': 'Depth [m]',
-            'b': 'Bottom Width [m]',
-            'So': 'Channel Slope [m/m]',
-            'z': 'Side Slope',
-            'D': 'Diameter [m]'
-        }
-        self.entries = {}
-
-        self.disable = {
-            'Rectangle': ['z', 'D'],
-            'Triangle': ['D', 'b'],
-            'Trapezoid': ['D'],
-            'Circle': ['z', 'b'],
-        }
-        for i, value in enumerate(self.values):
-            exclude = self.disable[section] if section != None else ['z', 'D', 'b']
-            calc = 'Q' if calculation == 'Q' else 'yn'
-            if value == calc:
-                continue
-            entry_frame = tk.Frame(self)
-            entry_frame.grid(row=1, column=i, padx=10, pady=(10, 0), sticky="w")
-            entry_label = tk.Label(entry_frame, text=definition[value])
-            entry_label.grid(row=0)
-            entry = CustomEntry(entry_frame, placeholder=value)
-            if value in exclude: 
-                entry.configure(state='disabled') 
-            entry.grid(row=1)
-            self.entries[value] = [entry, entry_label]
-
-    def show_entries(self, exclude):
-        for value in self.entries:
-            entry = self.entries[value][0]
-            disabled_values = self.disable[exclude]
-            if value not in disabled_values: 
-                entry.configure(state='normal') 
-            else:
-                entry.delete(0, 'end')
-                entry.put_placeholder()
-                entry.configure(state='disabled') 
-
-    def change_calculation(self, calc):
-        old_calc = 'yn' if calc == 'Q' else 'Q'
-        self.entries[old_calc] = self.entries[calc]
-        text = 'Altura' if calc == 'Q' else 'Caudal'
-        self.entries[old_calc][0].placeholder=old_calc
-        self.entries[old_calc][0].set_placeholder()
-        self.entries[old_calc][1].configure(text=text)
-        del self.entries[calc]
-
-
-
-    def get(self):
-        entry_values = {}
-        entry_labels = {}
-        for key, entry in self.entries.items():
-            entry_values[key] = entry[0].get()
-            entry_labels[key] = entry[1].cget('text')
-        return entry_values
-    
-class Selecciones(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        selecciones_label = tk.Label(self, text='Select Channel Type:', anchor='center')
-        selecciones_label.grid(row=0, column=0, sticky="ns" )
-        self.tipo_de_canal = ttk.Combobox(self, values=['Rectangle', 'Trapezoid', 'Triangle', 'Circle'], font="Arial 12")
-        self.tipo_de_canal.current(1)
-        self.tipo_de_canal.grid(row=0, column=1, padx= 30)
-
-        self.calculo_var = tk.StringVar(value="yn")
-
-        self.tipo_de_calculo_frame = tk.Frame(self)
-        self.tipo_de_calculo_frame.grid(row=0, column=2)
-        tipo_de_calculo = tk.Radiobutton(self.tipo_de_calculo_frame, value='yn', text='Calculate Depth', variable=self.calculo_var)
-        tipo_de_calculo2 = tk.Radiobutton(self.tipo_de_calculo_frame, value='Q', text='Calculate Flow', variable=self.calculo_var)
-        tipo_de_calculo.grid(row=0, column=2, sticky="w")
-        tipo_de_calculo2.grid(row=1, column=2, sticky="w")
-
-    def get(self):
-        return self.tipo_de_canal.get(), self.calculo_var.get()
-
-    def set(self, value):
-        self.calculo_var.set(value)
-
-class Canvas(tk.Canvas):
-    def __init__(self, master):
-        super().__init__(master, width=300, height=300)
-
-    def show_image(self):
-        img = Image.open('channel.png')
-        img = img.resize((215, 215))
-        self.pimg = ImageTk.PhotoImage(img)
-        self.create_image(0, 0, anchor='nw', image=self.pimg)
-
-
-
-    def draw_channel(self, section):
-        channel = section.get('channel_type')
-        yn = float(section.get('y'))
-        b = section.get('b')
-        z = section.get('z')
-        diameter = section.get('D')
-        angle = degrees(section.get('theta')) if section.get('theta') != None else None
-        match channel:
-            case 'Trapezoidal':
-                channel = draw_Trapezoid_channel(yn=yn, z=z, b=b)
-            case 'Circular':
-                channel = draw_circle(diameter=diameter, angle=angle)
-            case 'Rectangular':
-                channel = draw_Rectangle(yn=yn, b=b)
-            case 'Triangular':
-                channel = draw_triangle(yn=yn, z=z)
-        channel.save_as('channel.svg')
-        #converts svg to png
-        get_img_from_draw(channel)
-        # #a file called channel.png is created
-        self.show_image()
-
-        
-
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title("Hydrosolve")
-        self.geometry("650x300")
+        self.geometry("680x300")
         self.grid_columnconfigure((0, 1), weight=1)
 
         self.title_label = tk.Label(self, text='Hydrosolve', justify='center', height=2, font=('Helvetica 16 bold'))
         self.title_label.grid(row=0, columnspan=2)
 
-        self.selecciones = Selecciones(self)
-        self.selecciones.grid(row=1, columnspan=2)
-        self.selecciones.tipo_de_canal.bind("<<ComboboxSelected>>", self.show_enabled)
-        self.selecciones.calculo_var.trace('w', self.select_calc)
 
+        self.flow_type_label = tk.Label(self, text='Flow type: ', justify='left')
+        self.flow_type_label.grid(row=1)
+        self.flow_type = ttk.Combobox(self, values=['Open Channel Flow', 'Pipe Flow'], font="Arial 12")
+        self.flow_type.bind("<<ComboboxSelected>>", self.select_flow)
+        self.flow_type.current(0)
+        self.flow_type.grid(row=1, column=1)
 
-        self.parameters = tk.Frame(self)
-        self.parameters.grid(row=2, columnspan=2)
-        self.section = 'Trapezoid'
-        self.calc = 'yn'
-
-        self.hydraulic_parameters = MyEntries(self.parameters, values=['n', 'So', 'Q', 'yn'])
-        self.hydraulic_parameters.grid(row=0, column=0)
-
-        self.geometric_parameters = MyEntries(self.parameters, values=['b', 'z', 'D'], section=self.section)
-        self.geometric_parameters.grid(row=1, column=0)
+        self.open_flow = OpenFlowGui(self)
+        self.pipe_flow = PipeFlow(self)
         
-        self.button = ttk.Button(self, text="Calc Value", command=self.button_callback)
-        self.button.grid(row=4, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-        self.results_frame = tk.Frame(self)
-        self.results_frame.grid(row=5, columnspan=2)
-
-        self.canvas = Canvas(self.results_frame)
-        self.canvas.grid(row=0, column=0, padx=(80, 0))
-        self.canvas.create_text(101, -93, text="z", fill="black", font=('Helvetica 14 bold'))
-        self.canvas.create_text(116, -110, text="1", fill="black", font=('Helvetica 14 bold'))
-        self.canvas.create_polygon(110, -100, 110, -120, 90, -100)
-
-        self.results_data = tk.Frame(self.results_frame)
-        self.results_data.grid(row=0, column=1, sticky='w')
-        self.results = tk.Label(self.results_data, text='', width=25, justify='left')
-        self.results.grid(row=1, column=0, sticky='w')
-
-    
-    def show_enabled(self, event):
-        self.section = self.selecciones.get()[0]
-        self.geometric_parameters.show_entries(self.section)
-
-    def select_calc(self, *args):
-        self.calc = self.selecciones.calculo_var.get()
-        self.hydraulic_parameters.change_calculation(self.calc)
+        self.select_flow(event=None)
 
 
-    def more_info_callback(self):
-        result_window = ResultsWindow(self.calculated_section.__dict__)
+    def select_flow(self, event):
+        selected_flow = self.flow_type.get()
+        if selected_flow == 'Open Channel Flow':
+            self.open_flow.grid(row=2, columnspan=2)
+            self.pipe_flow.grid_forget()
+        elif selected_flow == 'Pipe Flow':
+            self.pipe_flow.grid(row=2, columnspan=2)
+            self.open_flow.grid_forget()
 
 
-    def button_callback(self):
-        hydraulic_params = self.hydraulic_parameters.get()
-        geometric_params = self.geometric_parameters.get()
-        section, calculo = self.selecciones.get()
-        n_input = float(hydraulic_params['n'])
-        So_input = float(hydraulic_params['So'])
-        Q_input = float(hydraulic_params['Q']) if calculo != "Q" and 'Q' in  hydraulic_params else None
-        y_input = float(hydraulic_params['yn']) if calculo != "yn" else Symbol('y')
-        b_input = float(geometric_params['b']) if not geometric_params['b'].isalpha()  else None
-        z_input = float(geometric_params['z']) if not geometric_params['z'].isalpha()  else None
-        D_input = float(geometric_params['D']) if not geometric_params['D'].isalpha()  else None
-        self.calculated_section = utils.calculate_section(section, n_input, So_input, Q_input, b_input, z_input, D_input, y_input)
-        self.canvas.draw_channel(self.calculated_section.__dict__)
-        self.results_title = tk.Label(self.results_data, text='Results: ', width=25, justify='left', font=("Arial", 16), anchor="w")
+class PipeFlow(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.results_title = tk.Label(self, text='PIPE gui', width=25, justify='left', font=("Arial", 16), anchor="w")
         self.results_title.grid(row=0, column=0, sticky='n')
-        self.results.config(text = utils.formater_str(self.calculated_section.__dict__, ['n', 'So', 'Q', 'y', 'z', 'D', 'b']), font=("Arial", 12), anchor="w")
-        self.more_results_button = tk.Button(self, text="More info", command=self.more_info_callback)
-        self.more_results_button.grid(row=6, column=0,  padx=20, pady=0, sticky="ew" ,columnspan=2)
 
-        self.geometry('650x630')
-
-
-class ResultsWindow(tk.Toplevel):
-    def __init__(self, section):
-        super().__init__() 
-        self.geometry('600x500')
-        self.section = section
-        
-        # self.first_results = ttk.Frame(self)
-        # self.first_results.grid(row=0, column=0)
-
-        self.canvas = Canvas(self)
-        self.canvas.draw_channel(section)
-        self.canvas.grid(row=0, column=0)
-        self.result = tk.Label(self, text=utils.formater_str(section, ['channel_type','n', 'So', 'Q', 'z', 'D', 'b']), font=("Arial", 12), anchor="w", justify='left')
-        self.result.grid(row=0, column=1)
-        self.result2 = tk.Label(self, text=utils.formater_str(section, ['y', 'a', 'rh', 'dh', 'tw', 'p']), font=("Arial", 12), anchor="w", justify='left')
-        self.result2.grid(row=1, column=0)
-        self.result3 = tk.Label(self, text=utils.formater_str(section, [ 'f', 'v', 'flow_status',  'zc', 'yn', 'Sc']), font=("Arial", 12), anchor="w", justify='left')
-        self.result3.grid(row=1, column=1)
-
-        self.print_results_button = ttk.Button(self, text='Show in Excel', command=self.open_excel)
-        self.print_results_button.grid(row=2, column=0,  padx=20, pady=0, sticky="ew" ,columnspan=2)
-
-    def open_excel(self):
-        generate_report(self.section)
 
 
 
---
+++ gui_pipe_flow.py
@@ -0,0 +1,165 @@
+import tkinter as tk
+from tkinter import ttk
+from custom_components import CustomEntry
+
+import sys
+sys.path.append('modules')
+
+import utils
+from excel_export import generate_report
+
+# Ahora puedes usar las funciones y variables definidas en modulo.py
+
+
+class Entries(tk.Frame):
+    def __init__(self, master, values, section = None, calculation=None):
+        super().__init__(master)
+        self.values = values 
+        self.section = section
+        definition = {
+            'f': "Darcy Friction Factor",
+            'Q': 'Flow Rate [m3/s]',
+            'L': 'Length [m]',
+            'ID': 'Internal Diameter [m]',
+            'C': 'Hazen-Williams Coef.',
+            'e': 'Surface Roughness [m]'
+        }
+        self.entries = {}
+
+        self.disable = {
+            'Darcy-Weisbach': ['C'],
+            'Hazen-Williams': ['e'],
+        }
+
+
+        for i, value in enumerate(self.values):
+            entry_frame = tk.Frame(self)
+            entry_frame.grid(row=1, column=i, padx=10, pady=(10, 0), sticky="w")
+            entry_label = tk.Label(entry_frame, text=definition[value])
+            entry_label.grid(row=0)
+            entry = CustomEntry(entry_frame, placeholder=value)
+            if value == 'C' and self.section == 'Darcy-Weisbach':
+                entry.config(state='disabled')
+            if value == 'e' and self.section == 'Hazen-Williams':
+                entry.config(state='disabled')
+            entry.grid(row=1)
+            self.entries[value] = [entry, entry_label]
+
+    def show_entries(self, exclude):
+        for value in self.entries:
+            entry = self.entries[value][0]
+            disabled_values = self.disable[exclude]
+            if value not in disabled_values: 
+                entry.configure(state='normal') 
+            else:
+                entry.delete(0, 'end')
+                entry.put_placeholder()
+                entry.configure(state='disabled') 
+
+
+
+    def get(self):
+        entry_values = {}
+        entry_labels = {}
+        for key, entry in self.entries.items():
+            entry_values[key] = entry[0].get()
+            entry_labels[key] = entry[1].cget('text')
+        return entry_values
+    
+
+        
+
+class PipeFlowGui(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.title("Hydrosolve")
+        self.geometry("650x300")
+        self.grid_columnconfigure((0, 1), weight=1)
+
+        self.title_label = tk.Label(self, text='Hydrosolve', justify='center', height=2, font=('Helvetica 16 bold'))
+        self.title_label.grid(row=0, columnspan=2)
+
+
+        self.flow_type_label = tk.Label(self, text='Flow type: ', justify='left')
+        self.flow_type_label.grid(row=1)
+        self.flow_type = ttk.Combobox(self, values=['Open Channel Flow', 'Pipe Flow'], font="Arial 12")
+        self.flow_type.current(1)
+        self.flow_type.grid(row=1, column=1)
+
+        # self.selecciones = Selecciones(self)
+        # self.selecciones.grid(row=1, columnspan=2)
+        # self.selecciones.tipo_de_canal.bind("<<ComboboxSelected>>", self.show_enabled)
+        # self.selecciones.calculo_var.trace('w', self.select_calc)
+
+
+        self.parameters = tk.Frame(self)
+        self.parameters.grid(row=2, columnspan=2)
+        # self.section = 'Trapezoid'
+        # self.calc = 'yn'
+
+        self.pipe_parameters = Entries(self.parameters, values=['Q', 'ID', 'L'])
+        self.pipe_parameters.grid(row=0, column=0)
+
+
+
+        self.method_frame = tk.Frame(self)
+        self.method_frame.grid(row=3, columnspan=3, pady=10)
+
+        self.method_type_label = tk.Label(self.method_frame, text='Calculation Method: ')
+        self.method_type_label.grid(row=0, column=0)
+        self.method_type = ttk.Combobox(self.method_frame, values=['Darcy-Weisbach', 'Hazen-Williams'], font="Arial 12")
+        self.method_type.current(0)
+        self.section = 'Darcy-Weisbach'
+        self.method_type.grid(row=0, column=1)
+        self.method_parameters = Entries(self.method_frame, values=['C', 'e'],section=self.section)
+        self.method_parameters.grid(row=1, columnspan=2)
+        self.method_type.bind("<<ComboboxSelected>>", self.show_enabled)
+
+
+
+        # self.method_parameters = Entries(self.parameters, values=['b', 'z', 'ID'], section=self.section)
+        # self.method_parameters.grid(row=1, column=0)
+        
+        self.button = ttk.Button(self, text="Calc Value", command=self.pipe_button_callback)
+        self.button.grid(row=4, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+
+        self.results_frame = tk.Frame(self)
+        self.results_frame.grid(row=5, columnspan=2)
+
+
+        self.results_data = tk.Frame(self.results_frame)
+        self.results_data.grid(row=0, column=1, sticky='w')
+        self.results = tk.Label(self.results_data, text='', width=35, justify='left')
+        self.results.grid(row=1, column=0, sticky='w')
+
+    
+    def show_enabled(self, event):
+        self.section = self.method_type.get()
+        self.method_parameters.show_entries(self.section)
+        print(self.section)
+
+    def select_calc(self, *args):
+        self.calc = self.selecciones.calculo_var.get()
+        self.pipe_parameters.change_calculation(self.calc)
+
+
+    def pipe_button_callback(self):
+        pipe_params = self.pipe_parameters.get()
+        method_params = self.method_parameters.get()
+        print(method_params, pipe_params, self.section)
+        L_input = float(pipe_params['L'])
+        ID_input = float(pipe_params['ID'])
+        Q_input = float(pipe_params['Q']) 
+        e_input = float(method_params['e']) if self.section != 'Hazen-Williams' and 'e' in  method_params else None
+        C_input = float(method_params['C']) if self.section != 'Darcy-Weisbach' and 'C' in  method_params else None
+        self.calculated_pipe = utils.calculate_pipe(self.section, L_input,  ID_input, Q_input,  e_input, C_input )
+        self.results_title = tk.Label(self.results_data, text='Results: ', width=25, justify='left', font=("Arial", 16), anchor="w")
+        self.results_title.grid(row=0, column=0, sticky='n')
+        self.results.config(text = utils.formater_str(self.calculated_pipe.__dict__), font=("Arial", 12), anchor="w")
+        # self.more_results_button = tk.Button(self, text="More info", command=self.more_info_callback)
+        # self.more_results_button.grid(row=6, column=0,  padx=20, pady=0, sticky="ew" ,columnspan=2)
+
+        self.geometry('650x570')
+
+
