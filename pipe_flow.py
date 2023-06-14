73035ef5973e103be1f89b023aa54cdf1afc957a
Juan David <juanguerrero09mc@gmail.com>
2023-06-14 16:15:57 +0200

    add pipe features

--- pipe_flow.py
+++ pipe_flow.py
@@ -7,49 +7,57 @@ G = 9.81 #gravity
 nu_20c = 0.0000010533 #kinematic viscosity at 20 C
 
 class Pipe:
-    def __init__(self, Q, D, e, L= None, n=None, rho = None, mu = None, nu = None, C = None, method= 'Darcy-Weisbach'):
+    def __init__(self, Q, D, e= None, L= None, rho = None, mu = None, C = None, method= 'Darcy-Weisbach'):
         self.Q = Q
         self.D = D
+        self.a = self.D**2 * pi / 4
         self.e = e
-        self.eD = self.e / self.D
+        self.eD = self.e / self.D if (e and D) != None else None
         self.v = Q / (pi * D**2 /4)
         self.nu = rho / mu if (rho and mu) != None else nu_20c
         self.Re = self.v * self.D / self.nu
+        self.L = L
         self.method = method
         self.C = C
-        if(self.Re < 2000):
-            self.flow_type = 'Laminar flow'
-            self.f = 64 / self.Re
-        elif (self.Re < 4000):
-            self.flow_type = 'Transitional flow'
-            self.f = self.calc_friction()
-        elif (self.Re >= 4000):
-            self.flow_type = 'Turbulent flow'
-            self.f = self.calc_friction()
         self.h = self.calc_head_loss()
+        self.hf = self.h * self.L
+        self.h = round(self.h, 4)
+        self.hf = round(self.hf, 4)
 
-    def calc_friction(self):
-        f = fluids.friction_factor(self.Re, self.eD)
-        return f
+
     def calc_head_loss(self):
         match self.method:
             case 'Darcy-Weisbach':
-                h = self.f * self.v**2 / (2 * self.D * G)
+                def calc_friction(self):
+                   return fluids.friction_factor(self.Re, self.eD)
+                if(self.Re < 2000):
+                    self.flow_type = 'Laminar flow'
+                    self.fr = 64 / self.Re
+                elif (self.Re < 4000):
+                    self.flow_type = 'Transitional flow'
+                    self.fr = calc_friction(self)
+                elif (self.Re >= 4000):
+                    self.flow_type = 'Turbulent flow'
+                    self.fr = calc_friction(self)
+                h = self.fr * self.v**2 / (2 * self.D * G)
+                self.fr = round(self.fr, 4)
                 return h
             case 'Hazen-Williams':
-                h = 10.674 * self.Q**1.852 / (self.D**4.78 * self.C**1.852)
+                h = 10.674 * self.Q**1.852 / (self.D**4.87 * self.C**1.852)
                 return h
 
-pipe1 = Pipe(1, 0.2, 0.0015, method='Hazen-Williams', C=150)
-print(pipe1.__dict__)
-        
+# pipe1 = Pipe(Q=1, D=0.9, e=0.005, L=100)
+# print(formater_str(pipe1.__dict__, ['Q', 'D', 'a', 'e', 'eD', 'v', 'nu', 'Re', 'L', 'method', 'flow_type', 'fr', 'h', 'hf']))
+# pipe1 = Pipe(Q=1, D=0.9, L=100, method='Hazen-Williams', C=140)
+# print(formater_str(pipe1.__dict__, ['Q', 'D', 'a', 'v', 'nu', 'Re', 'L', 'method', 'C', 'flow_type', 'fr', 'h', 'hf']))
 
 # Perdidas de carga h 
 
 """
 Q-> Caudal
 D-> Diametro interno
-e -> coef rugosidad del material
+e -> coef rugosidad absoluta del material
+eD -> coef rugosidad relativa del material
 Darcy (f, L?, D, Q, g)
 Manning (n, D, Q, L)
 Hazen-Williams (Q, C, D, L)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from open_flow import RectangularChannel, TrapezoidalChannel, TriangularChannel, CircularChannel
+from pipe_flow import Pipe
 from sympy import Symbol
 
 definitions = {
@@ -21,21 +22,42 @@ definitions = {
             'zc': 'Section Factor',
             'yc': 'Critical Depth',
             'Sc': 'Critical Slope',
+            'ID': 'Internal Diameter [m]',
+            'e': 'Surface Roughness [m]',
+            'eD': 'Relative Roughness',
+            'Re': 'Reynolds Number',
+            'L': 'Length [m]',
+            'method': 'Calculation Type',
+            'C': 'Hazen-Williams Coef.',
+            'flow_type': 'Flow Regime',
+            'fr': 'Darcy Friction Factor',
+            'h': 'Loss per meter [m/m]',
+            'hf':'Major Loss [m]'
 }
+
+'e' 'eD' 'Re' 'L' 'method' 'C' 'flow_type' 'f' 'h' 'hf'
 
 def calculate_section(section_input, n_input, So_input, Q_input=None, b_input=None, z_input=None, D_input= None, y_input = Symbol('y')):
     if section_input == "Rectangle":
-        section = RectangularChannel(n_input, So_input, Q_input, b_input, y_input)
+        section = RectangularChannel(n=n_input, So=So_input, Q=Q_input, b=b_input, y=y_input)
 
     elif section_input == "Triangle":
-        section = TriangularChannel(n_input, So_input, Q_input, z_input, y_input)
+        section = TriangularChannel(n=n_input, So=So_input, Q=Q_input, z=z_input, y=y_input)
 
     elif section_input == "Trapezoid":
-        section = TrapezoidalChannel(n_input, So_input, Q_input, b_input, z_input, y_input)
+        section = TrapezoidalChannel(n=n_input, So=So_input, Q=Q_input, b=b_input, z=z_input, y=y_input)
 
     elif section_input == "Circle":
-        section = CircularChannel(n_input, So_input, Q_input, D_input, y_input)
+        section = CircularChannel(n=n_input, So=So_input, Q=Q_input, D=D_input, y=y_input)
     return section
+
+def calculate_pipe(calc_type, L_input, ID_input, Q_input, e_input = None, C_input = None):
+    if calc_type == 'Darcy-Weisbach':
+        pipe = Pipe(Q_input, ID_input, e_input, L_input)
+    elif calc_type == 'Hazen-Williams':
+        pipe = Pipe(Q_input, ID_input, L = L_input, C= C_input, method= calc_type)
+    print(pipe.__dict__)
+    return pipe
 
 def trim_decimals(value):
     result_value = str(value)
@@ -52,7 +74,7 @@ def trim_decimals(value):
 def formater_str(results, selection = None):
     formated_results = ''
     for value in definitions:
-        if value in results:
+        if value in results and results[value] != None:
             if selection != None and value not in selection:
                 continue
             formated_results += definitions[value]
